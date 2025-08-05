d7891e683ddf9a5772130e85aebfcec754fa5bbf
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2025-08-05 19:27:43 +0400

    create files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VolunteersModule } from './volunteers/volunteers.module';
+import { AssignmentsModule } from './assignments/assignments.module';
 
 @Module({
-  imports: [],
+  imports: [VolunteersModule, AssignmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
-}
+TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'admin',
+  password: 'admin',
+  database: 'volunteersystem',
+  autoLoadEntities: true,
+  synchronize: false,
+}),
+
+  async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT ?? 3000);
+  }
 bootstrap();
+function bootstrap() {
+  throw new Error('Function not implemented.');
+}
+
