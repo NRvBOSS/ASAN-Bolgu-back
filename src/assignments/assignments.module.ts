e9f965962835deab8c9b806dd1b9ece901207da3
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2025-08-08 20:51:24 +0400

    reset backend, start Express

--- src/assignments/assignments.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AssignmentsService } from './assignments.service';
-import { AssignmentsController } from './assignments.controller';
-
-@Module({
-  providers: [AssignmentsService],
-  controllers: [AssignmentsController]
-})
-export class AssignmentsModule {}
