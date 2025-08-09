747142105e9aa7b5d207e0b83994ea9e32fb64de
NRvCODERPRO <140975114+NRvCODERPRO@users.noreply.github.com>
2025-08-09 18:22:32 +0400

    write app.js

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-const express = require("express");
+require("dotenv").config();
 
-// Express
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const volunteerRoutes = require("./routes/volunteerRoutes");
+const activityRoutes = require("./routes/activityRoutes");
+
+// Express app
 const app = express();
 
-// Listen on port 4000
-app.listen(4000, () => {
-  console.log("Server is running on port 4000");
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use((req, res, next) => {
+  console.log(req.path, res.method);
+  next();
 });
+
+// routes
+app.use("/api/volunteers", volunteerRoutes);
+app.use("/api/activities", activityRoutes);
+
+// connecting to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("Connecting to Db and listening on port", process.env.PORT);
+    });
+  })
+  .catch((err) => console.log(err));
